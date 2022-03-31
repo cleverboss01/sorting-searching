6cbab9886fce0d2cdbb91955a710c267b26214ff
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-03-31 16:00:00 +0100

    final sort

--- sortcheckpoint.js
+++ sortcheckpoint.js
@@ -5,19 +5,16 @@
 
 
 // Function to sort an array using insertion sort
-function insertionSort(arr, n)
-{
+const insertionSort = (arr, n) => {
 	let i, key, j;
-	for (i = 1; i < n; i++)
-	{
+	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
 
 		/* Move elements of arr[0..i-1], that are
 		greater than key, to one position ahead
 		of their current position */
-		while (j >= 0 && arr[j] > key)
-		{
+		while (j >= 0 && arr[j] > key) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
@@ -26,18 +23,16 @@ function insertionSort(arr, n)
 }
 
 // A utility function to print an array of size n
-function printArray(arr, n)
-{
+const printArray = (arr, n) => {
 	let i;
 	for (i = 0; i < n; i++)
-		document.write(arr[i] + " ");
-	document.write("<br>");
+		console.log(arr[i]);
 }
 
 // Driver code
-	let arr = [12, 11, 13, 5, 6 ];
-	let n = arr.length;
+let arr = [12, 11, 13, 5, 6, 23, 45, 1, 3, 89];
+let n = arr.length;
 
-	insertionSort(arr, n);
-	printArray(arr, n);
+insertionSort(arr, n);
+printArray(arr, n);
 
